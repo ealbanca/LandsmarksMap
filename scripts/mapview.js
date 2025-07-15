91ff81b38281f08053c60589d0b4d545e1b6e2d5
ealbanca <ealbanca@byui.edu>
2025-07-14 18:41:28 -0600

    Updated markers with Google maps Api

--- scripts/mapview.js
+++ scripts/mapview.js
@@ -1,12 +1,41 @@
-require(["esri/Map", "esri/views/MapView"], function (Map, MapView) {
-    const map = new Map({
-        basemap: "streets-navigation-vector" // you can change this basemap style
+function initMap() {
+    const idahoFalls = { lat: 43.4917, lng: -112.0341 };
+
+    const map = new google.maps.Map(document.getElementById("map"), {
+        center: idahoFalls,
+        zoom: 13,
     });
 
-    const view = new MapView({
-        container: "viewDiv",
-        map: map,
-        center: [-112.0341, 43.4917], // Longitude, latitude of Idaho Falls
-        zoom: 12
+    const markers = [
+        {
+            position: { lat: 43.4936, lng: -112.0402 },
+            title: "Idaho Falls Zoo at Tautphaus Park",
+            type: "park",
+        },
+        {
+            position: { lat: 43.4974, lng: -112.0341 },
+            title: "Museum of Idaho",
+            type: "museum",
+        },
+        {
+            position: { lat: 43.4953, lng: -112.0328 },
+            title: "Colonial Theater (Historic)",
+            type: "historical",
+        },
+    ];
+
+    const icons = {
+        park: "http://maps.google.com/mapfiles/ms/icons/green-dot.png",
+        museum: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png",
+        historical: "http://maps.google.com/mapfiles/ms/icons/red-dot.png",
+    };
+
+    markers.forEach(({ position, title, type }) => {
+        new google.maps.Marker({
+            position,
+            map,
+            title,
+            icon: icons[type],
+        });
     });
-});
+}
