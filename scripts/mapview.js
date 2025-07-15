9b75bc095506b575a1a15cd6a86b9081a50a165c
ealbanca <ealbanca@byui.edu>
2025-07-14 20:12:09 -0600

    Changed markers to become from a Json file

--- scripts/mapview.js
+++ scripts/mapview.js
@@ -6,36 +6,23 @@ function initMap() {
         zoom: 13,
     });
 
-    const markers = [
-        {
-            position: { lat: 43.4936, lng: -112.0402 },
-            title: "Idaho Falls Zoo at Tautphaus Park",
-            type: "park",
-        },
-        {
-            position: { lat: 43.4974, lng: -112.0341 },
-            title: "Museum of Idaho",
-            type: "museum",
-        },
-        {
-            position: { lat: 43.4953, lng: -112.0328 },
-            title: "Colonial Theater (Historic)",
-            type: "historical",
-        },
-    ];
-
-    const icons = {
-        park: "http://maps.google.com/mapfiles/ms/icons/green-dot.png",
-        museum: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png",
-        historical: "http://maps.google.com/mapfiles/ms/icons/red-dot.png",
-    };
-
-    markers.forEach(({ position, title, type }) => {
-        new google.maps.Marker({
-            position,
-            map,
-            title,
-            icon: icons[type],
-        });
-    });
+    fetch('markers.json')
+        .then(response => response.json())
+        .then(markers => {
+            markers.forEach(({ position, title, type }) => {
+                new google.maps.Marker({
+                    position,
+                    map,
+                    title,
+                    icon: icons[type],
+                });
+            });
+        })
+        .catch(error => console.error('Error loading marker data:', error));
 }
+
+const icons = {
+    park: "http://maps.google.com/mapfiles/ms/icons/green-dot.png",
+    museum: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png",
+    historical: "http://maps.google.com/mapfiles/ms/icons/red-dot.png",
+};
